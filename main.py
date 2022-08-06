dd2ff7c85618a1c52b8cec88423a104e89ff9392
DmitryEllison <71434912+DmitryEllison@users.noreply.github.com>
2022-08-06 20:26:48 +0300

    Update and rename delete.py to main.py
    adding a multiply operation

--- main.py
+++ main.py
@@ -2,16 +2,21 @@ import random
 
 level = int(input("Выберите уровень: "))
 print("Для остановки напишите \"стоп\".")
+
+typeOfProgram = input("Сделайте выбор \"+\" или \"*\"!")
+                
+while typeOfProgram != "+" or typeOfProgram != "*":
+    typeOfProgram = input("Сделайте выбор \"+\" или \"*\"!")
 print("Вводите ответ в формате {число}/{число}. Например, \"1/2\".")
+
 answer = []
 rating = 0
 
 while True:
-    numbers = [random.randint(1, 10 ** level) for i in range(4)]
-    if numbers[0] == numbers[1] or numbers[2] == numbers[3]:
-        continue
-    print('{}/{} + {}/{}'.format(numbers[0], numbers[1], numbers[2], numbers[3]))
-    result = round(numbers[0] / numbers[1] + numbers[2] / numbers[3], 5)
+    numbers = [random.randint(1, 10 ** level) for i in range(2*(1 + level))]
+    print('{}/{} {} {}/{}'.format(numbers[0], numbers[1], typeOfProgram, numbers[2], numbers[3]))
+    result = round(numbers[0] / numbers[1] + numbers[2] / numbers[3], 5) if typeOfProgram == "+" \
+        else round(numbers[0] / numbers[1] * numbers[2] / numbers[3], 5) 
 
     answer = input("Введите ответ: ")
 
@@ -28,8 +33,8 @@ while True:
             print("\t\033[32m{}\033[0m".format("Правильно!"))
             rating += 3 * level
         else:
-            print("\t\033[31m{}\033[0m".format("Неправильно!"))
+            print("\t\033[31mНеправильно, ответ {}!\033[0m".format(result))
             rating -= 1
+        print("\t\033[34m\tВаш рейтинг: {}\033[0m".format(rating))
     except:
         print("\tSomething went wrong!")
-    print("\t\033[34m{}{} \n\tВаш рейтинг: {}\033[0m".format("Ответ: ", round(result, 5), rating))
